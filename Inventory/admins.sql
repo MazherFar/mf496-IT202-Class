6148cf849aed02d7f03cfa582d95671cc9c93462
Mazher Farooq <mazherfarooq@mazhers-air.resource.campus.njit.edu>
2024-09-10 11:04:12 -0400

    Created admins.sql

---
+++ Inventory/admins.sql
@@ -0,0 +1,33 @@
+SHOW DATABASES;
+
+CREATE TABLE admins (
+ adminID      INT(11)      NOT NULL   AUTO_INCREMENT,
+ emailAddress VARCHAR(255) NOT NULL   UNIQUE,
+ password     CHAR(64)     NOT NULL,
+ firstName    VARCHAR(60)  NOT NULL,
+ lastName     VARCHAR(60)  NOT NULL,
+  PRIMARY KEY (adminID)
+);
+
+SHOW TABLES;
+
+
+SHOW CREATE TABLE admins;
+
+DESCRIBE admins;
+
+INSERT INTO admins
+(emailAddress, password, firstName, lastName)
+VALUES
+('taylor@guitarshop.com',
+SHA2('myL0ngP@ssword', 256), 'Taylor', 'Swift');
+
+SELECT * FROM admins;
+
+SELECT emailAddress, firstName FROM admins ORDER BY firstName;
+
+SELECT * FROM admins WHERE lastName = 'Swift';
+
+INSERT INTO admins (emailAddress, password, firstName, lastName) VALUES ('Bob@guitarshop.com', SHA2('THEL0ngP@ssword', 256), 'Bob', 'TheBuilder');
+
+UPDATE admins SET emailAddress = 'taylor.swift@guitarshop.com' WHERE adminID = 1;
