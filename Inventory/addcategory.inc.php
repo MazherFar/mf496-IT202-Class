e20e6f57ccbd7e6a9691ab49a866abbc8a12ae64
Mazher Farooq <mazherfarooq@mazhers-air.resource.campus.njit.edu>
2024-10-22 10:38:06 -0400

    u

--- Inventory/addcategory.inc.php
+++ Inventory/addcategory.inc.php
@@ -5,8 +5,8 @@ if (isset($_SESSION['login'])) {
    if ((trim($categoryID) == '') or (!is_numeric($categoryID))) {
        echo "<h2>Sorry, you must enter a valid category ID number</h2>\n";
    } else {
-       $categoryCode = $_POST['categoryCode'];
-       $categoryName = $_POST['categoryName'];
+       $categoryCode = htmlspecialchars($_POST['categoryCode']);
+       $categoryName = htmlspecialchars($_POST['categoryName']);
        $category = new Category($categoryID, $categoryCode, $categoryName);
        $result = $category->saveCategory();
        if ($result)
