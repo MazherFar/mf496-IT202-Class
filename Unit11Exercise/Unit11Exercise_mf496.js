1a8d179c179a991005db92eac944b6a4ea9b4a12
Mazher Farooq <mazherfarooq@mazhers-air.resource.campus.njit.edu>
2024-11-14 10:29:50 -0500

    a

---
+++ Unit11Exercise/Unit11Exercise_mf496.js
@@ -0,0 +1,29 @@
+// Mazher Farooq, IT-202 section 003, Internet Applications, Unit 11 In-Class Exercise, mf496@njit.edu
+function validateCents() {
+    const calculateButton = document.getElementById("calculate");
+    const centsInput = document.getElementById("cents");
+    let cents = centsInput.value;
+    console.log(`cents = ${cents}`);
+    if (cents.trim() === "" || isNaN(cents) || cents < 0 || cents > 99) {
+      centsInput.style.color = "red";
+      calculateButton.disabled = true;
+    } else {
+      centsInput.style.color = "black";
+      calculateButton.disabled = false;
+    }
+}
+function makeChange() {
+    let cents = parseInt(document.getElementById("cents").value);
+    const quarters = parseInt(cents / 25);  // get number of quarters
+    console.log(`quarters = ${quarters}`);
+    cents = cents % 25;                     // assign remainder to cents variable
+    const dimes = parseInt(cents / 10);     // get number of dimes
+    cents = cents % 10;                     // assign remainder to cents variable
+    const nickels = parseInt(cents / 5);    // get number of nickels
+    const pennies = cents % 5;              // get number of pennies
+    // display the results of the calculations
+    document.getElementById("quarters").value = quarters;
+    document.getElementById("dimes").value = dimes;
+    document.getElementById("nickels").value = nickels;
+    document.getElementById("pennies").value = pennies;
+}
